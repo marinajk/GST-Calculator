cf4f26f6d673f2b9474993c8461345700b274a77
Marina John <marinajohnkennedy@gmail.com>
2019-02-11 17:37:07 +0530

    Done

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
- 
-  function calculate()
-{  
-    var t = document.getElementsByClassName("textboxes");
-   var perform=(keyObj)=>
-   
-    {
-        if((((gst.value)==null) && ((np.value)==null))|| (((gp.value)==null) &&((ta.value)==null))|| (((gp.value)==null)&& ((np.value)==null)) ||(((ta.value)==null)&& ((np.value)==null) )||(((gst.value)==null)&& ((gp.value)==null)) || (((gst.value)==null)&& ((ta.value)==null)))
+var t = document.getElementsByClassName("textboxes");
+
+var perform=(keyObj)=>
         {
-            gst.value="";
-            np.value="";
-            gp.value="";
-            ta.value="";
-        }
-        else
-        {
-            if(((gst.value)!=null) && ((np.value)!=null))
+             if(((np.value)!=null) && ((gst.value)!=null) )
             {
                 ta.value=(((gst.value)/100)*(np.value));
                 gp.value=parseFloat(ta.value)+parseFloat(np.value);   
             }
              else if(((gp.value)!=null) && ((gst.value)!=null))
             {
-                ta.value=((gp.value)*((gst.value)/100));
+                ta.value=(((gst.value)/100)*(gp.value));
                 np.value=((gp.value)-(ta.value));
             }
              else if(((gp.value)!=null) && ((ta.value)!=null))
             {
                 np.value=(gp.value)-(ta.value);
-                alert(np.value);
                 gst.value=(((ta.value)*100)/(np.value));
             }
             else if(((ta.value)!=null) && ((np.value)!=null))
             {
-                gp.value=(parseFloat(np.value)+parseFloat(ta.value));
+                gp.value=parseFloat(np.value)+parseFloat(ta.value);
                 gst.value=(((ta.value)*100)/(np.value));
             }
             else if(((np.value)!=null) && ((gp.value)!=null))
@@ -43,13 +30,13 @@
              else if(((gst.value)!=null) && ((ta.value)!=null))
             {
                 np.value=(((ta.value)*100)/(gst.value));
-                gp.value=(parseFloat(np.value)+parseFloat(ta.value));
+                gp.value=parseFloat(np.value)+parseFloat(ta.value);
             }
         }
-   }
 
     for (let i=0;i<t.length;i++)
 {
     t[i].addEventListener("keypress",perform,false);
+
 }
-}
+ 
