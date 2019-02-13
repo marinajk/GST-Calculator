06e9823b0210dbfcb0dec1d9b7b2adc6e8ec665a
Marina John <marinajohnkennedy@gmail.com>
2019-02-13 19:23:43 +0530

    Design done.

--- index.js
+++ index.js
@@ -1,42 +1,96 @@
-var t = document.getElementsByClassName("textboxes");
+var t = document.getElementsByClassName("texts");
+var perform=()=>
+{
+    var np1 = false;
+    var gst1 = false;
+    var gp1 = false;
+    var ta1 = false;
+    
+    if (np.value != null)
+        np1 = true;
+    if (gst.value != null)
+        gst1 = true;
+    if (gp.value != null)
+        gp1 = true;
+    if (ta.value != null)
+        ta1 = true;
 
-var perform=(keyObj)=>
+    if(np1)
+    {
+        if(gst1)
         {
-             if(((np.value)!=null) && ((gst.value)!=null) )
-            {
-                ta.value=(((gst.value)/100)*(np.value));
-                gp.value=parseFloat(ta.value)+parseFloat(np.value);   
-            }
-             else if(((gp.value)!=null) && ((gst.value)!=null))
-            {
-                ta.value=(((gst.value)/100)*(gp.value));
-                np.value=((gp.value)-(ta.value));
-            }
-             else if(((gp.value)!=null) && ((ta.value)!=null))
-            {
-                np.value=(gp.value)-(ta.value);
-                gst.value=(((ta.value)*100)/(np.value));
-            }
-            else if(((ta.value)!=null) && ((np.value)!=null))
-            {
-                gp.value=parseFloat(np.value)+parseFloat(ta.value);
-                gst.value=(((ta.value)*100)/(np.value));
-            }
-            else if(((np.value)!=null) && ((gp.value)!=null))
-            {
-                ta.value=((gp.value)-(np.value));
-                gst.value=(((ta.value)*100)/(np.value));
-            }
-             else if(((gst.value)!=null) && ((ta.value)!=null))
-            {
-                np.value=(((ta.value)*100)/(gst.value));
-                gp.value=parseFloat(np.value)+parseFloat(ta.value);
-            }
+            ta.value=(((gst.value)/100)*(np.value));
+            gp.value=parseFloat(ta.value)+parseFloat(np.value); 
+        }
+        else if(gp1)
+        {
+            ta.value=((gp.value)-(np.value));
+            gst.value=(((ta.value)*100)/(np.value));
+        }
+        else if(ta1)
+        {
+            gp.value=parseFloat(np.value)+parseFloat(ta.value);
+            gst.value=(((ta.value)*100)/(np.value));
+        }
+    }
+    else  if(gst1)
+    {
+        if(np1)
+        {
+            ta.value=(((gst.value)/100)*(np.value));
+            gp.value=parseFloat(ta.value)+parseFloat(np.value);
+        }
+        else if(gp1)
+        {
+            ta.value=(((gst.value)/100)*(gp.value));
+            np.value=((gp.value)-(ta.value));
+        }
+        else if(ta1)
+        {
+            np.value=(((ta.value)*100)/(gst.value));
+            gp.value=parseFloat(np.value)+parseFloat(ta.value);
+        }
+    }
+  else if(gp1)
+    {
+        if(np1)
+        {
+            ta.value=((gp.value)-(np.value));
+            gst.value=(((ta.value)*100)/(np.value));
+        }
+        else if(gst1)
+        {
+            ta.value=(((gst.value)/100)*(gp.value));
+            np.value=((gp.value)-(ta.value));
+        }
+        else if(ta1)
+        {
+            np.value=(gp.value)-(ta.value);
+            gst.value=(((ta.value)*100)/(np.value));
         }
 
-    for (let i=0;i<t.length;i++)
-{
-    t[i].addEventListener("keyup",perform,false);
+    }
+    else  if(ta1)
+    {
+        if(np1)
+        {
+            gp.value=parseFloat(np.value)+parseFloat(ta.value);
+            gst.value=(((ta.value)*100)/(np.value));
+        }
+        else if(gst1)
+        {
+            np.value=(((ta.value)*100)/(gst.value));
+            gp.value=parseFloat(np.value)+parseFloat(ta.value);
+        }
+        else if(gp1)
+        {
+            np.value=(gp.value)-(ta.value);
+            gst.value=(((ta.value)*100)/(np.value));
+        }
+    }
+}
+
+for (let i = 0; i < t.length; i++) {
+    t[i].addEventListener("keyup", perform, false);
 
 }
- 
